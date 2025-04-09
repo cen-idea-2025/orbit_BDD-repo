d0cb094bc78cecb8aaaf70916b13ef09f0c83d47
Ebuka Frank <ebukafrank@gmail.com>
2025-04-09 10:35:57 +0100

    added bunker test

---
+++ bunker_test/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Welcome to my World");
+let myName = "Chukwuebuka";
+let myAge = 34;
+let studentAges = ["Frank", "Emmanuel", "David", "Akachukwu"];
+console.log(studentAges);
---
+++ bunker_test/src/index.ts
@@ -0,0 +1,9 @@
+
+console.log("Welcome to my World");
+
+let myName: string = "Chukwuebuka";
+let myAge: number =34;
+let studentAges: string[]= ["Frank","Emmanuel","David","Akachukwu"];
+
+console.log(studentAges);
+
